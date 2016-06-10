e6a21d485a09f3f54ff107cb6bfcc00e5a723ae8
schelian <suhas.chelian@yahoo.com>
2016-06-10 15:46:07 -0700

    adding TMC objects -- tea bottle OBJ file is still rendered as textureless... generalized tea_bottle shell script some

--- tea_bottle_renderAndTrain.sh
+++ tea_bottle_renderAndTrain.sh
@@ -1,37 +1,42 @@
 #!/bin/sh
+# to run: sh ./tea_bottle.sh
 # render tea bottle with same rot. param's as plate; this is probably good for all objects symmetric around the z axis
+
+OBJ=tea_bottle
+CAD_FILENAME=17_Pet_bottle_pet_tea.obj
+
 
 echo "################################################################################"
 echo rendering
 echo "################################################################################"
-mkdir -p output/tea_bottle
-\rm -rf output/tea_bottle/*
-./render ./input/17_Pet_bottle_pet_tea.ply output/tea_bottle   10 15  1   3  6  1 # Rx 15 Ry 10 Rz 0
+mkdir -p output/$OBJ
+\rm -rf output/$OBJ/*
+./render ./input/$CAD_FILENAME output/$OBJ   10 15  1   3  6  1 # Rx 15 Ry 10 Rz 0
 
 # testing which axis is which
-#./render ./input/17_Pet_bottle_pet_tea.ply output/tea_bottle   90  1  1   2  1  1 # Rx 90
-#./render ./input/17_Pet_bottle_pet_tea.ply output/tea_bottle    1 90  1   1  2  1 # Ry 90
-#./render ./input/17_Pet_bottle_pet_tea.ply output/tea_bottle    1  1 90   1  1  2 # Rz 90
+#./render ./input/$CAD_FILENAME output/$OBJ   90  1  1   2  1  1 # Rx 90
+#./render ./input/$CAD_FILENAME output/$OBJ    1 90  1   1  2  1 # Ry 90
+#./render ./input/$CAD_FILENAME output/$OBJ    1  1 90   1  1  2 # Rz 90
 
 # copy renders for training
-mkdir -p ../vncc_train/input/tea_bottle
-\rm -rf ../vncc_train/input/tea_bottle/*
-cp output/tea_bottle/template*.* ../vncc_train/input/tea_bottle
+mkdir -p ../vncc_train/input/$OBJ
+\rm -rf ../vncc_train/input/$OBJ/*
+cp output/$OBJ/template*.* ../vncc_train/input/$OBJ
 
 # copy renders for testing
-mkdir -p ../realtime_vncc/models/tea_bottle
-\rm -rf ../realtime_vncc/models/tea_bottle/*
-cp output/tea_bottle/template*.* ../realtime_vncc/models/tea_bottle
+mkdir -p ../realtime_vncc/models/$OBJ
+\rm -rf ../realtime_vncc/models/$OBJ/*
+cp output/$OBJ/template*.* ../realtime_vncc/models/$OBJ
 
 # train
 echo "################################################################################"
 echo training
 echo "################################################################################"
 cd ../vncc_train
-mkdir -p ../vncc_train/output/tea_bottle
-\rm -rf ../output/tea_bottle/*
-\rm -rf ../pre/tea_bottle.yaml
-./train ./input/tea_bottle ./output/tea_bottle ./pre/tea_bottle.yaml
+mkdir -p ../vncc_train/output/$OBJ
+\rm -rf ../output/$OBJ/*
+\rm -rf ../pre/$OBJ.yaml
+./train ./input/$OBJ ./output/$OBJ ./pre/$OBJ.yaml
 
 # copy yaml for testing
-cp ./pre/tea_bottle.yaml ../realtime_vncc/pre
+cp ./pre/$OBJ.yaml ../realtime_vncc/pre
